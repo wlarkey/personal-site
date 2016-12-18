80946fd8a33c45d00a42928d1d9e145aa4fd6507
Wyatt Larkey <wlarkey@purdue.edu>
2016-12-17 22:33:27 -0500

    getting set up

---
+++ js/generate_homepage.js
@@ -0,0 +1,10 @@
+var content = '';
+var numOfArticles = 10;
+var counter = 0;
+
+while(counter < numOfArticles) {
+  content = content + "Hello There! <br />";
+  counter++;
+}
+
+document.write(content);
---
+++ js/generate_sidebar.js
@@ -0,0 +1,10 @@
+var content = '';
+var numOfArticles = 10;
+var counter = 0;
+
+while(counter < numOfArticles) {
+  content = content + "Sidebar! <br />";
+  counter++;
+}
+
+document.write(content);
