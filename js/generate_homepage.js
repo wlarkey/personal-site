5a699993916ba8205647b8c7f17f5e4904949182
Wyatt Larkey <wlarkey@purdue.edu>
2016-12-17 23:06:44 -0500

    dynamic creation of content

--- js/generate_homepage.js
+++ js/generate_homepage.js
@@ -1,10 +1,33 @@
-var content = '';
-var numOfArticles = 10;
-var counter = 0;
+function loadcontent() {
 
-while(counter < numOfArticles) {
-  content = content + "Hello There! <br />";
-  counter++;
+  var page = document.getElementById('mainpage');
+  var sidebar = document.getElementById('sidebar');
+  var content = '';
+
+  var numOfArticles = 10;
+  var numOfSide = 5;
+  var counter = 0;
+
+  while(counter < numOfArticles) {
+    content = content + '<div class="jumbotron" style="margin-right: 25px;"> Hello There! <br /> </div>';
+    counter++;
+  }
+
+  page.innerHTML = content;
+
+  // RESET COUNTERS
+  content = '';
+  counter = 0;
+
+  while(counter < numOfSide) {
+    content = content + '<div class="jumbotron"> SideBar! <br /> </div>';
+    counter++;
+  }
+
+  sidebar.innerHTML = content;
+
+  // RESET COUNTERS
+  content = '';
+  counter = 0;
+
 }
-
-document.write(content);
