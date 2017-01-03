ceae8ea96e9b3701de4f8f6d219316127fbc9bb7
Wyatt Larkey <wlarkey@purdue.edu>
2017-01-03 14:23:31 -0500

    All splash pages done

--- js/generate_demos.js
+++ js/generate_demos.js
@@ -1,7 +1,10 @@
 function loadDemo() {
 
   var page = document.getElementById('mainpage');
+  var opening = document.getElementById('welcome');
   var content = '';
+
+  var headline = 'Here are my voice acting demos and more creative works. Below I hope to maintain a blog of my progress with voice acting.';
 
   var article = 'Hello There!';
   var article_text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. ';
@@ -9,6 +12,22 @@ function loadDemo() {
   var numOfArticles = 10;
   var numOfSide = 5;
   var counter = 0;
+
+  // GENERATE OPENING CONTENT
+  content += '<h1>VO Demos</h1> <h3>' + headline + '</h3>' +
+    '<div class="container">' +
+      '<div id="pallette" class="row">' +
+        '<div id="animInPic">' +
+          '<img src="pics/wlarkey.jpg">' +
+        '</div>' +
+      '</div>' +
+    '</div>';
+
+  opening.innerHTML = content;
+
+  // RESET COUNTERS
+  content = '';
+  counter = 0;
 
   // GENERATE MAIN CONTENT
   while(counter < numOfArticles) {
--- js/generate_projects.js
+++ js/generate_projects.js
@@ -1,7 +1,10 @@
 function loadProj() {
 
   var page = document.getElementById('mainpage');
+  var opening = document.getElementById('welcome');
   var content = '';
+
+  var headline = 'Below are some projects I have been working on in my spare time. I will try to post update when I make significant progress on any of them.';
 
   var article = 'Hello There!';
   var article_text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. ';
@@ -9,6 +12,22 @@ function loadProj() {
   var numOfArticles = 10;
   var numOfSide = 5;
   var counter = 0;
+
+  // GENERATE OPENING CONTENT
+  content += '<h1>What am I working on?</h1> <h3>' + headline + '</h3>' +
+    '<div class="container">' +
+      '<div id="pallette" class="row">' +
+        '<div id="animInPic">' +
+          '<img src="pics/wlarkey.jpg">' +
+        '</div>' +
+      '</div>' +
+    '</div>';
+
+  opening.innerHTML = content;
+
+  // RESET COUNTERS
+  content = '';
+  counter = 0;
 
   // GENERATE MAIN CONTENT
   while(counter < numOfArticles) {
