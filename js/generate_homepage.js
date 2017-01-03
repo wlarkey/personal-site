eeaa149e12e7f2b55bdb746389998b7e671cda82
Wyatt Larkey <wlarkey@purdue.edu>
2017-01-03 00:40:26 -0500

    done with front page

--- js/generate_homepage.js
+++ js/generate_homepage.js
@@ -5,6 +5,9 @@ function loadcontent() {
   var opening = document.getElementById('welcome');
   var content = '';
 
+  var headline = '"The secret of getting ahead is getting started."';
+  var person = '- Mark Twain';
+
   var article = 'Hello There!';
   var article_text = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. ';
 
@@ -18,12 +21,14 @@ function loadcontent() {
   // GENERATE OPENING CONTENT
   content += '<h1>Hello World!</h1> <h3>Wyatt Larkey\'s Offical Webpage</h3>' +
     '<div class="container">' +
-      '<div class="row">' +
+      '<div id="pallette" class="row">' +
         '<div id="animInPic" class="col-lg-6 col-md-6">' +
           '<img src="pics/wlarkey.jpg">' +
         '</div>' +
         '<div id="animInText" class="col-lg-6 col-md-6 hidden-sm hidden-xs">' +
-          '<h3>Here is some text and Stuff...</h3>' +
+          '<h2> Quote of the Day</h2>' +
+          '<h3>' + headline + '</h3>' +
+          '<h4>' + person + '</h4>' +
         '</div>' +
       '</div>' +
     '</div>';
